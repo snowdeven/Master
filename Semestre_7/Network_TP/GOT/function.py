fd6e55c44f5b2ef742bde34dfb13fa98cdadf4e0
snowdeven <chiarieven@gmail.com>
2022-12-06 15:27:37 +0100

    after one day of work with Ali and corrections

--- Semestre_7/Network_TP/GOT/function.py
+++ Semestre_7/Network_TP/GOT/function.py
@@ -7,22 +7,32 @@ import os
 from settings import *
 
 
-def data(s):
+def Data(S):
+    """This function read the csv file for a season S 
+    using panda to compute the graph and to put the data in list.
+
+    Args:
+        s (int): the number of a season
+
+    Returns: 
+        Id (list): list of  type string of the season Id
+        Label (list): list of  type string of the season Label
+        HouseId (list): list of  type string of the season HouseId
+        Source (list): list of  type string of the season Source
+        Target (list): list of  type string of the season Target
+        Weight (list): list of integer off the season Weight
+        G (networkx.graph): Graph on a file with nodes, edges and labels
+    """    
+    path1=os.path.join(os.path.dirname(__file__), f'data/got-s{S}-nodes.csv') #we use os library to get the path of our file 
+    path2=os.path.join(os.path.dirname(__file__), f'data/got-s{S}-edges.csv')
     
-    i=str(s)
-
-    path1=os.path.join(os.path.dirname(__file__), 'data/got-s'+i+'-nodes.csv')
-    path2=os.path.join(os.path.dirname(__file__), 'data/got-s'+i+'-edges.csv')
-
-    x=pd.read_csv(path1)
+    x=pd.read_csv(path1) #we use panda to read csv file 
     y=pd.read_csv(path2)
-    G=nx.from_pandas_edgelist(y,"Source","Target")
     
-    x=x.to_numpy()
+    G=nx.from_pandas_edgelist(y,"Source","Target") #we create a graph using networkx from panda dataframe 
+    
+    x=x.to_numpy()#turn  panda dataframe to an array 
     y=y.to_numpy()
-    y=y[:,:3]
-    
-    
     
     Id=list(x[:,0])
     Label=list(x[:,1])
@@ -35,12 +45,115 @@ def data(s):
     return Id, Label, HouseId, Source, Target, Weight, G
 
 
+def Important_char(func,n,G):
+    """Find the most (n=5) important characters in terms of degree, closesnees or betweeness 
+    using a function 'func 'to represent each one of them
+    we compute the function of G and we get a dictonary
+    after we sorted it in decrasing order and take the n-first keys = characters
 
 
-def Create_graph(s,Weighted=False):
-    Id=data(s)[0]
-    Source=data(s)[3]
-    Target=data(s)[4]
+    Args:
+        func (function): represent func that can be compute our graph in our case it is degree, closesnees or betweeness
+        n (int): variable to choose how many we want for the most important character
+        G (networkx.granx.coph): Graph on a file with nodes, edges and labels
+
+    Returns:
+        Name (list) :list of  type string of (n=5) most important characters name
+        Value (list) :list of  type string of (n=5) most important characters value of the function "func"
+    """    
+    Name=["Name"]
+    Value=["Value"]
+    dic=func(G)
+    sorted_dic=sorted(dic.items(), key=lambda x:x[1],reverse=True)
+    for i in sorted_dic[:n]:
+        Name.append(i[0])
+        Value.append(i[1])
+
+    return Name,Value
+
+
+def Txt_imp_char(S,n,G):
+    """Create a txt file of (n=5) most important characters in terms of degree, closesnees or betweeness for the season s
+    Args:
+        s (int): the number of a season
+        n (int): variable to choose how many we want for the most important character
+        G (networkx.graph): Graph on a file with nodes, edges and labels
+
+    """    
+    a=Important_char(nx.degree_centrality,n,G)
+
+    b=Important_char(nx.closeness_centrality,n,G)
+
+    c=Important_char(nx.betweenness_centrality,n,G)
+    file = open(os.path.join(os.path.dirname(__file__),f"result/exercise_2/GOT_Most_important_S{S}.txt"),"w") #"w" to open a file and erase the previous data if he is already exsit
+    file = open(os.path.join(os.path.dirname(__file__),f"result/exercise_2/GOT_Most_important_S{S}.txt"),"a") #"a" to open a file and add the data after the previous one
+
+    Title="| degree_centrality |"
+    file.write("-"*len(Title)+"\n"+Title +"\n"+"-"*len(Title)+"\n")
+    for i in range(len(a[0])):
+        file.write(a[0][i]+","+str(a[1][i])+"\n") 
+
+    Title="| closeness_centrality |"
+    file.write("-"*len(Title)+"\n"+Title +"\n"+"-"*len(Title)+"\n")
+    for i in range(len(a[0])):
+        file.write(b[0][i]+","+str(b[1][i])+"\n")
+
+    Title="| betweeness_centrality |"
+    file.write("-"*len(Title)+"\n"+Title +"\n"+"-"*len(Title)+"\n")
+    for i in range(len(a[0])):
+        file.write(c[0][i]+","+str(c[1][i])+"\n")
+
+
+    file.close()
+
+def Txt_stats(S,G):
+    """Create a txt file for all the character with their values in terms of degree, closesnees or betweeness for the season s
+    Args:
+        s (int): the number of a season
+        n (int): variable to choose how many we want for the most important character
+        G (networkx.graph): Graph on a file with nodes, edges and labels
+    """    
+    dic1=nx.degree_centrality(G)
+    dic2=nx.closeness_centrality(G)
+    dic3=nx.betweenness_centrality(G)
+    
+
+    Name=Data(S)[0]
+    Value_d=[]
+    Value_c=[]
+    Value_b=[]
+    
+    file = open(os.path.join(os.path.dirname(__file__),f"result/exercise_2/GOT_stats_S{S}.txt"),"w")#"w" to open a file and erase the previous data if he is already exsit
+    file = open(os.path.join(os.path.dirname(__file__),f"result/exercise_2/GOT_stats_S{S}.txt"),"a")#"a" to open a file and add the data after the previous one
+    file.write("Name,Degree,Closeness,Betweenness"+"\n")
+    
+    for i in range(len(Name)): 
+        Value_d.append(dic1.get(Name[i])) #dic(1/2/3).get to obtains values of the key name[i]
+        Value_c.append(dic2.get(Name[i]))
+        Value_b.append(dic3.get(Name[i]))
+    
+
+        file.write(str(Name[i])+","+str(Value_d[i])+","+str(Value_c[i])+","+str(Value_b[i])+"\n")
+        
+    file.close()
+    
+
+def Mat_adj(S,Weighted=False):
+    """Define the adjacency matrix for a graph of a season s. 
+    the number of rows and column is the number of nodes,
+    each element (m[i,j]) of our matrix has to be equal to one if i and j are linked,
+    or equal to the weight of the link between the node i and j for the Weighted adjacency matrix.
+
+    Args:
+        s (int): the number of a season
+        Weighted (bool, optional): boolean to precise the tpe of matrix, weighted or not. Defaults to False.
+
+    Returns:
+        mat_adj (array) : matrix of adjacency
+    """    
+    Id=Data(S)[0]
+    Source=Data(S)[3]
+    Target=Data(S)[4]
     
     L_S=len(Source)
     L_I=len(Id)
@@ -49,61 +162,27 @@ def Create_graph(s,Weighted=False):
     for i in range(L_S):
         a=Id.index(Source[i])
         b=Id.index(Target[i])
-        # print(Source[i],Target[i],i)
-        # print(a+2,b+2)
-        # print(i)
+        
         if Weighted == True :
             
-            mat_adj[a,b]=data(s)[5][i]
-            mat_adj[b,a]=data(s)[5][i]
+            mat_adj[a,b]=Data(S)[5][i]
+            mat_adj[b,a]=Data(S)[5][i]
         else:
             mat_adj[a,b]=1
             mat_adj[b,a]=1
-    
-    G=nx.from_numpy_array(mat_adj) 
-    return mat_adj,G
+            
+    return mat_adj
 
 
-def Draw_graph(s,G):
-    
-    nx.draw_networkx(G)
-    plt.show()
-    
-
-def Max_count(func,n,G,s):
-    Name=["Name"]
-    Value=["Value"]
-    func=func(G)
-    it=len(func)
-    while len(func) != it-n :
-        x=max(func, key=func.get)
-        Name.append(x)
-        Value.append(round(func[x],3))
-        func.pop(x)
-    return Name, Value
-
-def stats(s,G):
-    a=Max_count(nx.degree_centrality,5,G,s)
-
-    b=Max_count(nx.closeness_centrality,5,G,s)
-
-    c=Max_count(nx.betweenness_centrality,5,G,s)
-    fichier = open(os.path.join(os.path.dirname(__file__),"GOT_Most_important"+str(s)+".txt"),"w")
-    fichier = open(os.path.join(os.path.dirname(__file__),"GOT_Most_important"+str(s)+".txt"),"a")
-
-    fichier.write("---------------------\n"+"| degree_centrality |\n"+"---------------------\n")
-    for i in range(len(a[0])):
-        fichier.write(a[0][i]+","+str(a[1][i])+"\n")
-
-
-    fichier.write("------------------------\n"+"| closeness_centrality |\n"+"------------------------\n")
-    for i in range(len(a[0])):
-        fichier.write(b[0][i]+","+str(b[1][i])+"\n")
-
-
-    fichier.write("--------------------------\n"+"| betweenness_centrality |\n"+"--------------------------\n")
-    for i in range(len(a[0])):
-        fichier.write(c[0][i]+","+str(c[1][i])+"\n")
-
-
-    fichier.close()
+def Hist_Weight(S):
+    """Create a histogram of of the links weight for the season S.
+    Args:
+        S (int): the number of a season
+    """    
+    plt.title(f"season_{S}")
+    w=Data(S)[5]
+    bindwith=1
+    bin = range(min(w), max(w)+ bindwith, bindwith)
+    plt.hist(w,bins=bin,color="c",edgecolor="black")
+    plt.xlabel("Weight")
+    plt.ylabel("Frequency")
--- Semestre_7/Network_TP/GOT/main.py
+++ Semestre_7/Network_TP/GOT/main.py
@@ -6,32 +6,79 @@ import os
 from settings import *
 from function import *
 
+# --------------PART_TWO--------------#
 
-s=8
-
-
-
-
-G=data(s)[6]
-
-# Draw_graph(s,G)
-
-
-
-
-
-
-
-
-
-
-for i in range(1,9):
-    loc="24"+str(i)
+for S in range(1,9):                         #we made a loop to compute all the data for each season
+    loc="24"+str(S)                          #localisation of the subplot
     plt.subplot(int(loc))
-    G=data(i)[6]
-    plt.title("season_"+str(i))
-    nx.draw(G,node_size=15)
-    stats(i,G)
+    G=Data(S)[6]                             #our function data that retrun the network in position 6
+    plt.title("season_"+str(S))
+    nx.draw(G,node_size=15,with_labels=False) #We draw the Graph
+    Txt_imp_char(S,5,G)          #We write a file of most important character of the season S
+    Txt_stats(S,G)                               #We write a file of all the stats for the character of the season S
+
+
+plt.savefig(os.path.join(os.path.dirname(__file__),f"result/exercise_2/Graph_S1-S8.png"))
+plt.show()
+
+
+# --------------PART_THREE--------------#
+
+
+
+for S in range(1,9):
+    Name=Data(S)[0]
+    W_degree=np.zeros(len(Name))
+
+    mat_adj=Mat_adj(S,Weighted=True)
+    for i in range(len(Name)):
+        W_degree[i]=sum(mat_adj[i,:])
+
+    sort_index=np.argsort(W_degree)[::-1]
+    if S == 1:
+        file = open(os.path.join(os.path.dirname(__file__),"result/exercise_3/Weighted_most_imp_char_S1-S9.txt"),"w")
+    else:
+        file = open(os.path.join(os.path.dirname(__file__),"result/exercise_3/Weighted_most_imp_char_S1-S9.txt"),"a")
+    Title=f"| Weighted_degree_centrality season {S} |"
+    file.write("-"*len(Title)+"\n"+Title +"\n"+"-"*len(Title)+"\n")
+    file.write("Name,Value\n")
+    for i in sort_index[:5]:
+        file.write(Name[i]+","+str(W_degree[i])+"\n")
+    
+    file = open(os.path.join(os.path.dirname(__file__),f"result/exercise_3/Char_weighted_degree_S{S}.txt"),"w")
+    file = open(os.path.join(os.path.dirname(__file__),f"result/exercise_3/Char_weighted_degree_S{S}.txt"),"a")
+    file.write("Name,Value\n")
+    for count, elem in enumerate(W_degree):
+        file.write(Name[count]+ f", {elem}"+"\n")
+    file.close()
+
+for S in range(1,9):#we made a loop to compute all the data for each season
+    Colors=["royalblue"]
+    loc=f"24{S}"   #localisation of the subplot
+    plt.subplot(int(loc))
+    Hist_Weight(S)
+
+
+plt.savefig(os.path.join(os.path.dirname(__file__),f"result/exercise_3/Weight_distribution_S1-S8.png"))
 
 plt.show()
+
+# --------------PART_FOUR-------------- #
+
+for S in range(1,9):
+    G=Data(S)[6]
+
+    C=nx.connected_components(G)
+
+
+    size=[len(c) for c in sorted(nx.connected_components(G), key=len, reverse=True)]
+
+    relative_size=np.array(size)/sum(size)
+    file = open(os.path.join(os.path.dirname(__file__),f"result/exercise_4/Connected_compt_table_S{S}.txt"),"w")
+    file = open(os.path.join(os.path.dirname(__file__),f"result/exercise_4/Connected_compt_table_S{S}.txt"),"a")
+    file.write(f"Number of Connected_components : {len(list(C))}\n")
+    for island in relative_size:
+        file.write(f"relative size : {island}\n")
+    file.close()
+
 
